80bddc3e7de0a73020c05239f6f303f591fd7a76
Godfrey Nolan <godfrey@riis.com>
2015-09-24 16:42:24 -0400

    Second screen now displays login info

--- app/src/main/java/com/riis/hellopreferences/MainActivity.java
+++ app/src/main/java/com/riis/hellopreferences/MainActivity.java
@@ -1,27 +1,54 @@
 package com.riis.hellopreferences;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-    private static String USERLOGIN = "godfreynolan";
+    EditText usernameField, passwordField, emailField;
+    private static String REGISTERED = "true";
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        usernameField = (EditText) findViewById (R.id.username_field);
+        passwordField = (EditText) findViewById(R.id.password_field);
+        emailField = (EditText) findViewById(R.id.email_field);
 
         UserPreferences uPref = new UserPreferences();
-        uPref.saveSharedPreferences(this, USERLOGIN);
+        uPref.saveSharedPreferences(this, REGISTERED);
 
-        TextView myTextView = (TextView) findViewById(R.id.txt_field);
-        myTextView.setText(uPref.getSharedPreferences(this));
+        loginButton = (Button) findViewById(R.id.login_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onSubmit(v);
+            }
+        });
+     }
+
+
+    public void onSubmit(View v) {
+        // implicit intent
+        Intent intent = new Intent();
+
+        Bundle bundle = new Bundle();
+        bundle.putString("Username", usernameField.getText().toString());
+        bundle.putString("Password", passwordField.getText().toString());
+        bundle.putString("Email", emailField.getText().toString());
+        intent.setAction("com.riis.login.IntentReceiverActivity");
+
+        intent.addCategory(Intent.CATEGORY_DEFAULT);
+        intent.putExtras(bundle);
+        startActivity(intent);
+
     }
-
-
-
-
-
 }
